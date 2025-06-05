cfef61f1bc4153aa75d64faf72d52f16b53821ad
adri-cfb <adri_cfb@hotmail.com>
2025-06-06 01:37:53 +0200

    User & Role clases 1.0 (Validation and Enum)

---
+++ recipe-app-backend2025/src/main/java/com/recipe/recipe_app_backend2025/model/Role.java
@@ -0,0 +1,6 @@
+package com.recipe.recipe_app_backend2025.model;
+
+public class Role {
+    USER;
+    ADMIN
+}
---
+++ recipe-app-backend2025/src/main/java/com/recipe/recipe_app_backend2025/model/User.java
@@ -0,0 +1,48 @@
+package com.recipe.recipe_app_backend2025.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message= "Username is mandatory")
+    private String username;
+
+    @Email (message= "Email should be valid")
+    @NotBlank(message="Email in mandatory")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    @Enumerated (EnumType.STRING)
+    private Role role;
+
+    public User () {}
+
+    public User(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+
+    }
+
+    public Long getId() {return id;}
+
+    public String getUsername () {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public Role getRole() {return role;}
+    public void setRole(Role role) {this.role = role;}
+}
