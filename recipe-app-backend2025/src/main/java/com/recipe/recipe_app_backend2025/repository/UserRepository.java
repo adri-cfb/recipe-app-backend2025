d7804fe16ec0639b9b66238d58c0777669486b1b
adri-cfb <adri_cfb@hotmail.com>
2025-06-10 20:57:28 +0200

    UserRepository fixed

--- recipe-app-backend2025/src/main/java/com/recipe/recipe_app_backend2025/repository/UserRepository.java
+++ recipe-app-backend2025/src/main/java/com/recipe/recipe_app_backend2025/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.recipe.recipe_app_backend2025.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameyUsername(String username);
+    User findByUsername(String username);
     User findByEmail(String email);
 }
